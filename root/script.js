7eb3f0b4e21049d79261e11fb2f1e6761310795d
dennis <dennis34@students.cs.ubc.ca>
2022-01-16 03:10:55 -0800

    heroku 4

--- root/script.js
+++ root/script.js
@@ -24,7 +24,7 @@ document.getElementById("highscore").textContent = "High Score: " + highscore;
 playlistBtn.addEventListener(`click`, function(e) {
     e.preventDefault();
     playlist = [];
-    fetch(`/process_get?link=${playlistInput.value}`, {mode: 'no-cors'})
+    fetch(`https://nwencore.herokuapp.com/process_get?link=${playlistInput.value}`, {mode: 'no-cors'})
         .then((response) => {
             return response.json();
         })
@@ -42,7 +42,7 @@ playlistBtn.addEventListener(`click`, function(e) {
 playBtn.addEventListener("click", (e) => {
     e.preventDefault();
     playlist = [];
-    fetch(`/process_get?link=https://open.spotify.com/playlist/3XM4qNNOrn2PcaiyIe8nax?si=81ef02f784c64af7`, {mode: 'no-cors'}).then((response) => {
+    fetch(`https://nwencore.herokuapp.com/process_get?link=https://open.spotify.com/playlist/3XM4qNNOrn2PcaiyIe8nax?si=81ef02f784c64af7`, {mode: 'no-cors'}).then((response) => {
         console.log(response);
         return response.json();
     })
@@ -70,7 +70,7 @@ answerButton.addEventListener("click",
             restartGame();
         } else {
         let songInput = document.querySelector(`#song-input`).value;
-        fetch (`/song?guess=${songInput}&ans=${currentSong.track_name}`)
+        fetch (`https://nwencore.herokuapp.com/song?guess=${songInput}&ans=${currentSong.track_name}`)
             .then(response => response.json())
             .then(bool => {
                 clearInterval(intervalId);
